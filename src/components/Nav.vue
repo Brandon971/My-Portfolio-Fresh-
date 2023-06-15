4e0552ca4f71cf673f6c1e855fe7f83138bedd1b
Brandon971 <r.swinton971@gmail.com>
2023-06-15 12:00:17 +0200

    favicon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg bg-black sticky-top ">
   <div class="container-fluid">
      <img class="brandon-swinton-1" src="https://i.postimg.cc/7hHcW3NX/BRANDON.png" />
-    <a class="navbar-brand text-white" href="#">Mr.Swinton</a>
+    <a class="navbar-brand text-white" href="#">Portfolio</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
